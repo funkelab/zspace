de64ad886d1be53685f51619e6346280510131ad
Gabriela Barros <bgg@unc.edu>
2025-06-04 19:57:22 -0400

    feat: :sparkles: write a generating function for xc
    write a generating function for xc, simplify generating function for xa, add more self-documentation

--- notebooks/scratch.py
+++ notebooks/scratch.py
@@ -2,63 +2,45 @@
 import zspace
 import torch
 import numpy
-
-# %% [markdown] 
-# this is a markdown cell
+import random
 
 # %%
 def generate_xa(y):
     """
     Generate the image xa from the binary y.
     """
-
-    # initialize xa as an 24 x 24 img with 3 color components per pixel (RGB)
-    xa = torch.zeros(3, 24, 24)
+    num_components = 3   # RGB
+    height = 32
+    width = 32
+    xa = torch.zeros(num_components, height, width)
     dims = xa.shape
 
-    # create images from different normal distributions
-    irr_dist = torch.normal(mean=0, std=1, size=dims) # not dependent on y
-    rel_lvar_dist = torch.normal(mean=y, std=.1, size=dims) # dependent on y with low variance
-    rel_hvar_dist = torch.normal(mean=y, std=100, size=dims) # dependent on y with high variance
-    
-    dist_types = [irr_dist, rel_lvar_dist, rel_hvar_dist]
-
-    # R sampled from irrelevant vals,
-    # G sampled from relevant, low variance vals,
-    # B sampled from relevant, high variance vals
     for i in range(dims[0]):
         for j in range(dims[1]):
             for k in range(dims[2]):
-                xa[i,j,k] = dist_types[i][i,j,k]
-
-    # implement without creating three reference images (unnecessary)
-    # for i in range(dims[0]):
-    #     for j in range(dims[1]):
-    #         for k in range(dims[2]):
-    #             if i == 0: 
-    #                 xa[i,j,k] = torch.normal(mean=0, std=1)
-    #             elif i == 1:
-    #                 xa[i,j,k] = torch.normal(mean=y, std=0.1)
-    #             else:
-    #                 xa[i,j,k] = torch.normal(mean=y, std=10)
+                if i == 0:   # R sampled from irrelevant normal distribution
+                    xa[i,j,k] = torch.normal(mean=0, std=1, size=(1,)).item()
+                elif i == 1:   # G sampled from relevant, low variance normal distribution
+                    xa[i,j,k] = torch.normal(mean=y, std=0.1, size=(1,)).item()
+                else:   # B sampled from relevant, high variance vals
+                    xa[i,j,k] = torch.normal(mean=y, std=10, size=(1,)).item()
 
     return xa
-
-# %%
-print(generate_xa(1))
 
 # %%
 def generate_xb(y):
     """
     Generate the bar chart probabilities xb from the binary y.
     """
-    xb = torch.zeros(10) # 10 classifications
+    num_classes = 10
+    xb = torch.zeros(num_classes)
 
     for i in range(xb.size(dim=0)):
-        xb[i] = torch.normal(mean=0, std=1, size=(1,)).item()
+        xb[i] = random.random()   # assign random val to each class
 
-    xb = xb.softmax(dim=0)
+    xb = xb.softmax(dim=0)   # convert vals to probability distribution
 
+    # sort in ascending order if y = 0, descending order if y = 1
     dec = False
     if y == 1:
         dec = True
@@ -68,13 +50,41 @@ def generate_xb(y):
     return xb
 
 # %%
-print(generate_xb(0))
-print(generate_xb(1))
-
-# %%
 def generate_xc(y):
     """
     Generate the time series xc from the binary c.
     """
+    num_walks = 10
+    num_timepoints = 50
+    xc = torch.zeros(num_walks, num_timepoints)
+    dims = xc.shape
 
-    return 0
+    for i in range(dims[0]):
+         xc[i,0] = random.random()
+
+    bias = 0.1   # chance of moving in biased direction (dependent on y)
+
+    for i in range(dims[0]):
+        for j in range(1, dims[1]):
+            if random.random() < bias:
+                if y == 0:   # biased down if y = 0
+                        xc[i,j] = xc[i,j-1] + torch.normal(mean=-1, std=1, size=(1,)).item()
+                elif y == 1:   # biased up if y = 1
+                    xc[i,j] = xc[i,j-1] + torch.normal(mean=1, std=1, size=(1,)).item()
+            else:
+                    xc[i,j] = xc[i,j-1] + torch.normal(mean=0, std=1, size=(1,)).item()
+
+    # check how many walks ended in biased direction; adjust bias as needed
+    # want to choose bias s.t. most walks are biased with some exceptions
+    # num_biased = 0
+    # for i in range(dims[0]):
+    #      if y == 0 and xc[i,0] > xc[i,num_timepoints-1]:
+    #             num_biased += 1
+    #      elif y == 1 and xc[i,0] < xc[i,num_timepoints-1]:
+    #             num_biased += 1
+    # print(f'{num_biased} out of {num_walks} walks')
+
+    return xc
+
+# %%
+print(generate_xc(1))
