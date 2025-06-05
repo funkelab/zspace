73007c5733a167728d73068c1f15417f5be5a22d
Diane Adjavon <diane.adjavon@icloud.com>
2025-06-05 11:33:59 -0400

    test: :test_tube: Add testing for the new dataset

---
+++ tests/test_dataset.py
@@ -0,0 +1,11 @@
+from zspace.dataset import ToyModel
+import pytest
+
+
+def test_dataset_shapes():
+    dataset = ToyModel(0)
+    sample = dataset[0]
+
+    assert sample[0].shape == (3, 32, 32)  # xa shape
+    assert sample[1].shape == (10,)  # xb shape
+    assert sample[2].shape == (10, 50)  # xc shape
