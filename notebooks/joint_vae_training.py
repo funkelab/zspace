71d2cdca0debd6a05228348a65fbc2371ac9283e
Gabriela Barros <bgg@unc.edu>
2025-07-08 13:10:37 -0400

    feat: :sparkles: generate UMAP projection of latent space

--- notebooks/joint_vae_training.py
+++ notebooks/joint_vae_training.py
@@ -1,5 +1,5 @@
 # %%
-import numpy
+import numpy as np
 
 # %%
 import os
@@ -117,7 +117,7 @@ for epoch in tqdm(range(num_epochs)):
     total_self_modal_loss = 0
     total_cross_modal_loss = 0
 
-    total_reconst_loss = 0
+    total_reconst_loss = 0 
     total_kl_loss = 0
 
     for batch_idx, batch in enumerate(train_loader):
@@ -153,8 +153,8 @@ for epoch in tqdm(range(num_epochs)):
         self_modal_loss = reconst_xaa + reconst_xbb + reconst_xcc
         cross_modal_loss = reconst_xab + reconst_xac + reconst_xba + reconst_xbc + reconst_xca + reconst_xcb
 
-        reconst_loss = self_modal_loss + cross_modal_loss
-        kl_loss = kl_xa + kl_xb + kl_xc
+        reconst_loss = (self_modal_loss + cross_modal_loss)/9
+        kl_loss = (kl_xa + kl_xb + kl_xc)/3
 
         loss = reconst_loss + beta_loss*kl_loss
 
@@ -207,8 +207,12 @@ for epoch in tqdm(range(num_epochs)):
             reconst_xcb, _ = compute_loss(xc_sample, "xc", xb_sample, "xb")
             reconst_xcc, _ = compute_loss(xc_sample, "xc", xc_sample, "xc")
 
-            reconst_loss = reconst_xaa + reconst_xab + reconst_xac + reconst_xba + reconst_xbb + reconst_xbc + reconst_xca + reconst_xcb + reconst_xcc
-            kl_loss = kl_xa + kl_xb + kl_xc
+            self_modal_loss = reconst_xaa + reconst_xbb + reconst_xcc
+            cross_modal_loss = reconst_xab + reconst_xac + reconst_xba + reconst_xbc + reconst_xca + reconst_xcb
+
+            reconst_loss = (self_modal_loss + cross_modal_loss)/9
+            kl_loss = (kl_xa + kl_xb + kl_xc)/3
+
             loss = reconst_loss + beta_loss*kl_loss
 
             total_test_loss += loss.item()
@@ -248,79 +252,135 @@ axs[4].set_title("Cross-modal training loss")
 for i in range(rows*columns):
     axs[i].set_xlabel("Epoch")
     axs[i].set_ylabel("Loss")
-    axs[i].set_xlim(0, min(num_epochs, 250))
+    axs[i].set_xlim(0, min(num_epochs, 50))
 
 plt.tight_layout()
 plt.show()
 
 # %%
-# define reconstruction graph functions
-import matplotlib.pyplot as plt
-import torch
+# define reconstruction function
 
-def reconstruct(true_xs: dict[str, torch.Tensor], reconst_xs: dict[str, torch.Tensor], input: str):
-    fig, axs = plt.subplots(1, 3, figsize=(15, 4))
-    mods = ["xa", "xb", "xc"]
+def reconstruct(model: nn.Module, dataloader: DataLoader, input_mod: str):
+    batch = next(iter(dataloader))
+    xa = batch[0]
+    xb = batch[1]
+    xc = batch[2]
 
-    for i, mod in enumerate(mods):
-        x_true = true_xs[mod].cpu().detach()
-        x_hat = reconst_xs[mod].cpu().detach()
+    for i in range(batch_size):
+        xa_samp = xa[i].flatten().to(device)
+        xb_samp = xb[i].flatten().to(device)
+        xc_samp = xc[i].flatten().to(device)
 
-        ax = axs[i]
-
-        if mod == "xa" or mod == "xb":
-            ax.bar(range(len(x_true)), x_true.numpy(), alpha=0.6, label="True")
-            ax.bar(range(len(x_hat)), x_hat.numpy(), alpha=0.6, label="Reconstructed")
-            ax.set_title(f"{mod} from {input}")
-            ax.legend()
-
-        elif mod == "xc":
-            ax.plot(x_true.numpy().T, label="True")
-            ax.plot(x_hat.numpy().T, label="Reconstructed")
-            ax.set_title(f"{mod} from {input}")
-            ax.legend()
-
-    plt.tight_layout()
-    plt.show()
-
-# %%
-# sample xas, xbs, xcs, and decode in all three modalities
-
-model.eval()
-
-batch = next(iter(test_loader))
-xa = batch[0]
-xb = batch[1]
-xc = batch[2]
-
-for i in range(batch_size):
-    xa_samp = xa[i].flatten().to(device)
-    xb_samp = xb[i].flatten().to(device)
-    xc_samp = xc[i].flatten().to(device)
-
-    true_xs = {
-        "xa" : xa_samp,
-        "xb" : xb_samp,
-        "xc" : xc_samp
-    }
-
-    with torch.no_grad():
-        # xa input
-        mean, log_var = model.encoders["xa"](xa_samp)
-        z = model.reparameterize(mean, log_var)
-
-        xa_hat = model.decoders["xa"](z)
-        xb_hat = model.decoders["xb"](z)
-        xc_hat = model.decoders["xc"](z)
-
-        reconst_xs = {
-            "xa" : xa_hat,
-            "xb" : xb_hat,
-            "xc" : xc_hat
+        true_xs = {
+            "xa" : xa_samp,
+            "xb" : xb_samp,
+            "xc" : xc_samp
         }
 
-        reconstruct(true_xs, reconst_xs, "xa")
+        with torch.no_grad():
+            mean, log_var = model.encoders[input_mod](true_xs[input_mod])
 
+            z = model.reparameterize(mean, log_var)
+
+            xa_hat = model.decoders["xa"](z)
+            xb_hat = model.decoders["xb"](z)
+            xc_hat = model.decoders["xc"](z)
+
+            reconst_xs = {
+                "xa" : xa_hat,
+                "xb" : xb_hat,
+                "xc" : xc_hat
+            }
+
+            fig, axs = plt.subplots(1, 3, figsize=(15, 4))
+
+            mods = ["xa", "xb", "xc"]
+
+            for i, mod in enumerate(mods):
+                x_true = true_xs[mod].cpu().detach()
+                x_hat = reconst_xs[mod].cpu().detach()
+
+                ax = axs[i]
+
+                if mod in ["xa", "xb"]:
+                    ax.bar(range(len(x_true)), x_true.numpy(), alpha=0.6, label="True")
+                    ax.bar(range(len(x_hat)), x_hat.numpy(), alpha=0.6, label="Reconstructed")
+                    ax.set_title(f"{mod} from {input_mod}")
+                    ax.legend()
+
+                elif mod == "xc":
+                    ax.plot(x_true.numpy().T, label="True")
+                    ax.plot(x_hat.numpy().T, label="Reconstructed")
+                    ax.set_title(f"{mod} from {input_mod}")
+                    ax.legend()
+
+            plt.tight_layout()
+            plt.show()
 
 # %%
-# plot latent space (umap)
+# sample and reconstruct all data modalities
+
+model.eval()
+mods = ["xa", "xb", "xc"]
+for mod in mods:
+    reconstruct(model, test_loader, mod)
+
+# %%
+# plot latent space (UMAP)
+
+import umap
+import matplotlib.pyplot as plt
+import numpy as np
+
+all_zs = []
+all_labels = []
+
+mods = ["xa", "xb", "xc"]
+model.eval()
+
+with torch.no_grad():
+    for batch in test_loader:
+        xa, xb, xc, ys = batch
+        ys = ys.tolist()
+
+        xs = {
+            "xa": xa.to(device).flatten(start_dim=1),
+            "xb": xb.to(device).flatten(start_dim=1),
+            "xc": xc.to(device).flatten(start_dim=1)
+        }
+
+        for mod in mods:
+            x = xs[mod]
+            means, _ = model.encoders[mod](x)
+            all_zs.append(means.numpy())
+
+            for y in ys:
+                all_labels.extend([f"{mod} (y = {y})"])
+
+z_array = np.vstack(all_zs)
+umap_model = umap.UMAP(n_components=2, random_state=test_seed)
+z_umap = umap_model.fit_transform(z_array)
+
+unique_labels = sorted(set(all_labels))
+color_palette = plt.get_cmap("Paired")
+label_to_color = {}
+
+for i, label in enumerate(unique_labels):
+    color = color_palette(i)
+    label_to_color[label] = color
+
+colors = []
+for label in all_labels:
+    colors.append(label_to_color[label])
+
+
+plt.figure(figsize=(10, 6))
+plt.scatter(z_umap[:, 0], z_umap[:, 1], c=colors, alpha=0.2)
+for label, color in label_to_color.items():
+    plt.scatter([], [], color=color, label=label)
+plt.legend()
+plt.title("UMAP projection of latent space")
+plt.tight_layout()
+plt.show()
+
+# %%
